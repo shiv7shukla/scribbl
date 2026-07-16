3ba313b224638f20d62fd1c54195ff371241b32a
Shiv Shukla <shivshukla00514@gmail.com>
2026-07-16 12:28:56 +0000

    add: store provider

---
+++ app/providers/game-store-provider.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { type ReactNode, createContext, useState, useContext } from 'react'
+import { useStore } from 'zustand'
+import { createGameStore } from '../stores/game-store';
+import { gameStore } from '@/lib/types/types';
+
+export type GameStoreApi = ReturnType<typeof createGameStore>
+
+export const GameStoreContext = createContext<GameStoreApi | undefined>(
+  undefined,
+)
+
+export interface GameStoreProviderProps{
+    children: ReactNode
+}
+
+export const GameStoreProvider = ({
+  children,
+}: GameStoreProviderProps) => {
+  const [store] = useState(() => createGameStore())
+  return (
+    <GameStoreContext.Provider value={store}>
+      {children}
+    </GameStoreContext.Provider>
+  )
+}
+
+export const useCounterStore = <T,>(
+  selector: (store: gameStore) => T,
+): T => {
+  const gameStoreContext = useContext(GameStoreContext)
+  if (!gameStoreContext) {
+    throw new Error(`useCounterStore must be used within CounterStoreProvider`)
+  }
+
+  return useStore(gameStoreContext, selector)
+}
